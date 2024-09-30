73fa7c02dd1227a0619ba4ffab047865c3f2ef8a
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-09-30 12:10:12 +0530

    added timestamps

--- model/employeeModel.js
+++ model/employeeModel.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true},
+    email: { type: String, required: true },
     mobile: { type: String, required: true },
     designation: { type: String, required: true },
     gender: { type: String, required: true },
     course: { type: String, required: true },
     image: { type: String }
-});
+}, { timestamps: true }); 
 
 module.exports = mongoose.model('Employee', employeeSchema);
